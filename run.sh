688f57715eb1b8c153b85585bb0604553a24908e
Shimon Doodkin <helpmepro1@gmail.com>
2014-03-28 03:48:45 +0000

    add rates server

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 if which node >/dev/null; then
-   # echo exists
+   echo exists
 else
-    #echo does not exist
-    export PATH="$PATH:/opt/node/bin"
+   echo does not exist
+   export PATH="$PATH:/opt/node/bin"
 fi
 
 nohup node testserver.js mynodeapp 3>&1 2>&1 1>/dev/null </dev/null &
