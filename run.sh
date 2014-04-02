a40ab13209d9f1702106c6e12b7a55872e6fe79f
Shimon Doodkin <helpmepro1@gmail.com>
2014-04-02 06:43:27 +0000

    ask in serial

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f "$0")
+SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd $SCRIPTPATH
+SCRIPTPATH=$(dirname "$SCRIPT")
 
+cd $SCRIPTPATH
 if which node >/dev/null; then
    echo exists
 else
@@ -14,5 +14,3 @@ else
 fi
 
 nohup node testserver.js mynodeapp 3>&1 2>&1 1>/dev/null </dev/null &
-
-
